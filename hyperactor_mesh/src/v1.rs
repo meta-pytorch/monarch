6294814f7527817858eba1d153a8750ed7420db6
Riley Dulin <dulinr@meta.com>
2025-10-15 09:38:18 -0700

    Add stop for v1::ActorMesh (#1529)
    Summary:
    
    Implement `.stop()` for an ActorMesh. This sends a message to the ProcMeshAgent
    to stop the actors on all procs that the actors live on.
    
    This is done with a new "Destroy" message that is the parallel of CreateOrUpdate.
    It will be implemented for HostMesh next to terminate procs.
    In order to cast a Destroy message, it needs a context::Actor impl which it gets from
    an Instance object.
    
    If any actor is used after stop, it'll get a NotExist error.
    Requires supervision to be enabled in order to get that error correctly without a timeout.
    
    Reviewed By: mariusae
    
    Differential Revision: D84547919

--- hyperactor_mesh/src/v1.rs
+++ hyperactor_mesh/src/v1.rs
@@ -111,6 +111,12 @@ pub enum Error {
     )]
     ActorSpawnError { statuses: RankedValues<Status> },
 
+    #[error(
+        "error stopping actor mesh: statuses: {}",
+        RankedValues::invert(statuses)
+    )]
+    ActorStopError { statuses: RankedValues<Status> },
+
     #[error("error: {0} does not exist")]
     NotExist(Name),
 }
