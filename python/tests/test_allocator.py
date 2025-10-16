06974c0ec496e5798f1216748554dcd4dbb71d75
Robert Rusch <rusch@meta.com>
2025-10-16 09:34:38 -0700

    Fix up failing tests by adding Tcp::Localhost mode (#1479)
    Summary:
    
    This diff adds support for tls localhost to fix T232884876. I go into detail in https://fb.workplace.com/groups/1434680487636162/posts/1441470780290466/, but basically, the hostname can't be bound to for certain environments. This generally takes the approach discussed with mariusae, though I ended up doing localhost instead of unspecified. This is because the tests were already set up with localhost; it was just being ignored. 
    
    This is a bit of a short term fix, as I hope to follow up by allowing the hostname to get sent to workers while using unspecified locally. DNS is "more correctly" resolved on the worker machines, due to proxying and load balancing questions, but that's a bigger lift.
    
    Reviewed By: mariusae
    
    Differential Revision: D84094699

--- python/tests/test_allocator.py
+++ python/tests/test_allocator.py
@@ -607,7 +607,6 @@ class TestRemoteAllocator(unittest.IsolatedAsyncioTestCase):
 
     # Skipping test temporarily due to blocking OSS CI TODO: @rusch T232884876
     @pytest.mark.oss_skip  # pyre-ignore[56]: Pyre cannot infer the type of this pytest marker
-    @pytest.mark.skip("broken")
     async def test_torchx_remote_alloc_initializer_no_match_label_1_mesh(self) -> None:
         server = ServerSpec(
             name=UNUSED,
@@ -623,7 +622,10 @@ class TestRemoteAllocator(unittest.IsolatedAsyncioTestCase):
             ],
         )
         port = get_free_port()
-        with remote_process_allocator(addr=f"tcp!{get_sockaddr('localhost', port)}"):
+
+        with remote_process_allocator(
+            addr=f"tcp!{get_sockaddr('localhost', port)}",
+        ):
             with mock.patch(SERVER_READY, return_value=server):
                 initializer = TorchXRemoteAllocInitializer("local:///test", port=port)
                 allocator = RemoteAllocator(
@@ -640,7 +642,6 @@ class TestRemoteAllocator(unittest.IsolatedAsyncioTestCase):
 
     # Skipping test temporarily due to blocking OSS CI TODO: @rusch T232884876
     @pytest.mark.oss_skip  # pyre-ignore[56]: Pyre cannot infer the type of this pytest marker
-    @pytest.mark.skip("broken")
     async def test_torchx_remote_alloc_initializer_with_match_label(self) -> None:
         server = ServerSpec(
             name=UNUSED,
@@ -656,7 +657,10 @@ class TestRemoteAllocator(unittest.IsolatedAsyncioTestCase):
             ],
         )
         port = get_free_port()
-        with remote_process_allocator(addr=f"tcp!{get_sockaddr('localhost', port)}"):
+
+        with remote_process_allocator(
+            addr=f"tcp!{get_sockaddr('localhost', port)}",
+        ):
             with mock.patch(SERVER_READY, return_value=server):
                 initializer = TorchXRemoteAllocInitializer("local:///test", port=port)
                 allocator = RemoteAllocator(
