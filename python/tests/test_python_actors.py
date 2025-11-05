6d8b9f4ed0ba854fd21c78c6eab6728a2e8361b8
zdevito <zdevito@meta.com>
2025-11-05 10:27:30 -0800

    [Logging] Thread names to actor isntances
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/1731
    
    Add enough information to Instance objects so they can produce a qualified name for themselves. Example:
    
    ```
    "<root>.<tests.test_python_actors.Named the_name{'f': 0/2}>"
    ```
    
    The name is the full path through their creators to the root actor, along with class names and given names for each actor.
    
    Two follow ups:
    
    1. get this string into user logging loggers so any log they print includes what actor it is running under.
    
    2. Use this information when creating a supervision exceptions so that we producing a nicely formatted error message.
    ghstack-source-id: 321120714
    @exported-using-ghexport
    
    Differential Revision: [D85988370](https://our.internmc.facebook.com/intern/diff/D85988370/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D85988370/)!

--- python/tests/test_python_actors.py
+++ python/tests/test_python_actors.py
@@ -1731,3 +1731,23 @@ def test_setup_async() -> None:
     counter.incr.call().get()
     # Make sure no errors occur in the meantime
     time.sleep(10)
+
+
+class Named(Actor):
+    @endpoint
+    def report(self):
+        return context().actor_instance.creator, str(context().actor_instance)
+
+
+def test_instance_name():
+    cr, result = (
+        this_host()
+        .spawn_procs(per_host={"f": 2})
+        .spawn("the_name", Named)
+        .slice(f=0)
+        .report.call_one()
+        .get()
+    )
+    assert result == "<root>.<tests.test_python_actors.Named the_name{'f': 0/2}>"
+    assert cr.name == "root"
+    assert str(context().actor_instance) == "<root>"
