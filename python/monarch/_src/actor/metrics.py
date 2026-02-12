8b2026571c117fd181340f0499fdc6b6f147a1b9
Thomas Wang <thomasywang@fb.com>
2026-02-11 20:26:29 -0800

    Receive .stream in Rust
    Differential Revision: D91755413

--- python/monarch/_src/actor/metrics.py
+++ python/monarch/_src/actor/metrics.py
@@ -10,18 +10,11 @@
 Telemetry metrics for endpoint operations.
 
 This module defines histograms and counters used to track endpoint
-performance metrics. Metrics (call_one, choose, call) are now implemented in Rust
+performance metrics. Metrics (call_one, choose, call, stream) are now implemented in Rust
 """
 
 from monarch._src.actor.telemetry import METER
 from opentelemetry.metrics import Counter, Histogram
-
-
-# Histogram for measuring endpoint stream latency per yield
-endpoint_stream_latency_histogram: Histogram = METER.create_histogram(
-    name="endpoint_stream_latency.us",
-    description="Latency of endpoint stream operations per yield in microseconds",
-)
 
 
 # Histogram for measuring endpoint message size
@@ -33,11 +26,6 @@ endpoint_message_size_histogram: Histogram = METER.create_histogram(
 endpoint_broadcast_error_counter: Counter = METER.create_counter(
     name="endpoint_broadcast_error.count",
     description="Count of errors in endpoint broadcast operations",
-)
-
-endpoint_stream_throughput_counter: Counter = METER.create_counter(
-    name="endpoint_stream_throughput.count",
-    description="Count of endpoint stream invocations for throughput measurement",
 )
 
 endpoint_broadcast_throughput_counter: Counter = METER.create_counter(
