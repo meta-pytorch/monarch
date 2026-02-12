f81051f25e54bd640e78bab898ae8b3ccfe0e51b
Thomas Wang <thomasywang@meta.com>
2026-02-11 20:30:20 -0800

    Rust ActorEndpoint (#2459)
    Summary:
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/2459
    
    - Provide a way to call `PythonActorMesh.cast()` and `PythonActorMesh.as_supervisor()` from Rust
    - Provide a way to call MethodSpecifier.name()` from Rust
    - Move the logic in `ActorEndpoint._rref()` to standalone function `_dispatch_actor_rref`
    - Delete Python `ActorEndpoint`
    - Create Rust `ActorEndpoint`
      - Delegate adverb calls to the implementation from the `Endpoint` trait
      - Delegate `ActorEndpoint._propagate` to `monarch._src.actor.endpoint._do_propagate()`
      - Delegate `ActorEndpoint.create_message` to `monarch._src.actor.actor_mesh._create_endpont_message`
      - Delegate `ActorEndpoint._rref` to `_dispatch_actor_rref`
    - Create .pyi stubs
    - Migrate `endpoint_message_size` counter to Rust
    
    Reviewed By: mariusae, shayne-fletcher
    
    Differential Revision: D91755420

--- python/monarch/_rust_bindings/monarch_hyperactor/endpoint.pyi
+++ python/monarch/_rust_bindings/monarch_hyperactor/endpoint.pyi
@@ -18,12 +18,20 @@ from typing import (
     TypeVar,
 )
 
+from monarch._rust_bindings.monarch_hyperactor.actor import MethodSpecifier
+from monarch._rust_bindings.monarch_hyperactor.actor_mesh import ActorMeshProtocol
 from monarch._rust_bindings.monarch_hyperactor.context import Instance
-from monarch._rust_bindings.monarch_hyperactor.mailbox import PortRef
+from monarch._rust_bindings.monarch_hyperactor.mailbox import OncePortRef, PortRef
 from monarch._rust_bindings.monarch_hyperactor.pytokio import PythonTask
-from monarch._rust_bindings.monarch_hyperactor.shape import Extent
+from monarch._rust_bindings.monarch_hyperactor.shape import Extent, Shape
 from monarch._rust_bindings.monarch_hyperactor.supervision import SupervisionMonitor
 from monarch._rust_bindings.monarch_hyperactor.value_mesh import ValueMesh
+from typing_extensions import ParamSpec
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+Selection = Literal["all", "choose"]
 
 _T = TypeVar("_T")
 
@@ -127,3 +135,77 @@ def stream_collector(
         - ValueStream: An iterator that yields PythonTasks for each response.
     """
     ...
+@final
+class ActorEndpoint(Generic[P, R]):
+    def __init__(
+        self,
+        actor_mesh: ActorMeshProtocol,
+        method: MethodSpecifier,
+        shape: Shape,
+        mesh_name: str,
+        signature: Any | None = None,
+        proc_mesh: Any | None = None,
+        propagator: Any | None = None,
+    ) -> None: ...
+    def _call_name(self) -> Any:
+        """Something to use in InputChecker to represent calling this thingy."""
+        ...
+    def call(self, *args: P.args, **kwargs: P.kwargs) -> "Future[ValueMesh[R]]": ...
+    def choose(self, *args: P.args, **kwargs: P.kwargs) -> Future[R]:
+        """
+        Load balanced sends a message to one chosen actor and awaits a result.
+
+        Load balanced RPC-style entrypoint for request/response messaging.
+        """
+        ...
+    def call_one(self, *args: P.args, **kwargs: P.kwargs) -> Future[R]: ...
+    def stream(self, *args: P.args, **kwargs: P.kwargs) -> ValueStream:
+        """
+        Broadcasts to all actors and yields their responses as a stream / generator.
+
+        This enables processing results from multiple actors incrementally as
+        they become available. Returns an async generator of response values.
+        """
+        ...
+    def broadcast(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        """
+        Fire-and-forget broadcast to all actors without waiting for actors to
+        acknowledge receipt.
+
+        In other words, the return of this method does not guarrantee the
+        delivery of the message.
+        """
+        ...
+    def rref(self, *args: P.args, **kwargs: P.kwargs) -> R: ...
+    def _propagate(
+        self, args: Any, kwargs: Any, fake_args: Any, fake_kwargs: Any
+    ) -> Any: ...
+    def _fetch_propagate(
+        self, args: Any, kwargs: Any, fake_args: Any, fake_kwargs: Any
+    ) -> Any: ...
+    def _pipe_propagate(
+        self, args: Any, kwargs: Any, fake_args: Any, fake_kwargs: Any
+    ) -> Any: ...
+    def _send(
+        self,
+        args: tuple[Any, ...],
+        kwargs: dict[str, Any],
+        port: PortRef | OncePortRef | None = None,
+        selection: Selection = "all",
+    ) -> None:
+        """
+        Implements sending a message to the endpoint. The return value of the endpoint will
+        be sent to port if provided. If port is not provided, the return will be dropped,
+        and any exception will cause the actor to fail.
+
+        The return value is the (multi-dimension) size of the actors that were sent a message.
+        For ActorEndpoints this will be the actor_meshes size. For free-function endpoints,
+        this will be the size of the currently active proc_mesh.
+        """
+        ...
+    @property
+    def _name(self) -> MethodSpecifier: ...
+    @property
+    def _signature(self) -> Any: ...
+    @property
+    def _actor_mesh(self) -> ActorMeshProtocol: ...
