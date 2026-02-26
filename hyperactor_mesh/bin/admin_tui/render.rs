4a9454d8b894a3959ed6601debf0c32d054a22a6
Shayne Fletcher <shaynefletcher@meta.com>
2026-02-26 13:13:56 -0800

    admin tui: module skeleton for refactor
    Differential Revision: D94099392

---
+++ hyperactor_mesh/bin/admin_tui/render.rs
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+pub mod detail_pane;
+pub mod status_bar;
+pub mod tree_pane;
