bd4d5f5a0b8586a73942e70fd2dedb31859fc493
Marius Eriksen <meriksen@meta.com>
2026-05-11 12:25:08 -0700

    [hyperactor] introduce Gateway reachability state
    Add a Gateway module as the owner of shared reachability state for procs. Move ProcState default advertised location and forwarder behind Gateway, and route proc address construction and gateway flushing through it while preserving existing Proc::configured behavior.
    
    Differential Revision: [D104674509](https://our.internmc.facebook.com/intern/diff/D104674509/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D104674509/)!
    
    [ghstack-poisoned]

---
+++ hyperactor/src/gateway.rs
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//! Shared reachability state for Hyperactor procs.
+
+use std::fmt;
+use std::sync::Arc;
+use std::sync::RwLock;
+
+use crate::Location;
+use crate::ProcAddr;
+use crate::ProcId;
+use crate::mailbox::BoxedMailboxSender;
+use crate::mailbox::MailboxSender as _;
+
+/// Shared ingress, egress, and advertised reachability state for one or more procs.
+#[derive(Clone)]
+pub struct Gateway {
+    inner: Arc<GatewayState>,
+}
+
+struct GatewayState {
+    /// The location used when constructing routeable addresses for
+    /// newly bound refs.
+    default_location: RwLock<Location>,
+
+    /// Sender used to forward messages outside of the proc.
+    forwarder: BoxedMailboxSender,
+}
+
+impl Gateway {
+    pub(crate) fn configured(default_location: Location, forwarder: BoxedMailboxSender) -> Self {
+        Self {
+            inner: Arc::new(GatewayState {
+                default_location: RwLock::new(default_location),
+                forwarder,
+            }),
+        }
+    }
+
+    /// The gateway's default advertised location.
+    pub fn default_location(&self) -> Location {
+        self.inner.default_location.read().unwrap().clone()
+    }
+
+    /// Set the gateway's default advertised location.
+    pub fn set_default_location(&self, location: Location) {
+        *self.inner.default_location.write().unwrap() = location;
+    }
+
+    /// Construct a routeable proc address using this gateway's default location.
+    pub fn proc_addr(&self, proc_id: &ProcId) -> ProcAddr {
+        ProcAddr::new(proc_id.clone(), self.default_location())
+    }
+
+    pub(crate) fn forwarder(&self) -> &BoxedMailboxSender {
+        &self.inner.forwarder
+    }
+
+    pub(crate) async fn flush(&self) -> Result<(), anyhow::Error> {
+        self.inner.forwarder.flush().await
+    }
+}
+
+impl fmt::Debug for Gateway {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Gateway")
+            .field("default_location", &self.default_location())
+            .finish()
+    }
+}
