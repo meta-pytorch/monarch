e2ec86582542ba301e57288e4bc6799ca1f94a74
Marius Eriksen <meriksen@meta.com>
2026-06-12 13:57:04 -0700

    [hyperactor] route casts through typed multipart parts
    Pull Request resolved: https://github.com/meta-pytorch/monarch/pull/4229
    
    Switch multicast/cast routing to mutate typed multipart repr parts instead of `Bindings`. `ErasedUnbound` now carries multipart data, visitors update `PortRefRepr`, `OncePortRefRepr`, and `RankRepr`, and cast delivery targets the ordinary typed handler port. The old bind/unbind and indexed wrapper definitions remain for the cleanup commit.
    ghstack-source-id: 392955647
    @exported-using-ghexport
    
    Differential Revision: [D108442500](https://our.internmc.facebook.com/intern/diff/D108442500/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D108442500/)!

--- monarch_hyperactor/src/actor.rs
+++ monarch_hyperactor/src/actor.rs
@@ -2062,7 +2062,6 @@ mod tests {
     use hyperactor::accum::StreamingReducerOpts;
     use hyperactor::id::Label;
     use hyperactor::message::ErasedUnbound;
-    use hyperactor::message::Unbound;
     use hyperactor::testing::ids::test_port_id;
     use hyperactor_mesh::Error as MeshError;
     use hyperactor_mesh::host_mesh::host_agent::ProcState;
@@ -2075,7 +2074,7 @@ mod tests {
     use crate::actor::to_py_error;
 
     #[test]
-    fn test_python_message_bind_unbind() {
+    fn test_python_message_part_codec() {
         let reducer_spec = ReducerSpec {
             typehash: 123,
             builder_params: Some(wirevalue::Any::serialize(&"abcdefg12345".to_string()).unwrap()),
@@ -2096,16 +2095,22 @@ mod tests {
         };
         {
             let mut erased = ErasedUnbound::try_from_message(message.clone()).unwrap();
-            let mut bindings = vec![];
+            let mut ports = vec![];
             erased
-                .visit_mut::<reference::UnboundPort>(|b| {
-                    bindings.push(b.clone());
+                .visit_mut::<reference::PortRefRepr>(|b| {
+                    ports.push(b.clone());
                     Ok(())
                 })
                 .unwrap();
-            assert_eq!(bindings, vec![reference::UnboundPort::from(&port_ref)]);
-            let unbound = Unbound::try_from_message(message.clone()).unwrap();
-            assert_eq!(message, unbound.bind().unwrap());
+            assert_eq!(ports.len(), 1);
+            assert_eq!(ports[0].port_addr(), port_ref.port_addr());
+            assert_eq!(ports[0].reducer_spec(), port_ref.reducer_spec());
+            assert_eq!(
+                ports[0].get_return_undeliverable(),
+                port_ref.get_return_undeliverable()
+            );
+            assert!(!ports[0].unsplit());
+            assert_eq!(message, erased.deserialize::<PythonMessage>().unwrap());
         }
 
         let no_port_message = PythonMessage {
@@ -2119,16 +2124,18 @@ mod tests {
         };
         {
             let mut erased = ErasedUnbound::try_from_message(no_port_message.clone()).unwrap();
-            let mut bindings = vec![];
+            let mut ports = vec![];
             erased
-                .visit_mut::<reference::UnboundPort>(|b| {
-                    bindings.push(b.clone());
+                .visit_mut::<reference::PortRefRepr>(|b| {
+                    ports.push(b.clone());
                     Ok(())
                 })
                 .unwrap();
-            assert_eq!(bindings.len(), 0);
-            let unbound = Unbound::try_from_message(no_port_message.clone()).unwrap();
-            assert_eq!(no_port_message, unbound.bind().unwrap());
+            assert_eq!(ports.len(), 0);
+            assert_eq!(
+                no_port_message,
+                erased.deserialize::<PythonMessage>().unwrap()
+            );
         }
     }
 
